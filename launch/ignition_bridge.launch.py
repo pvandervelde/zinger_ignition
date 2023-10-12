4fda482f1ea48d6b02194ded2db47fb8ff250a35
Petrik van der Velde <petrikvandervelde@gmail.com>
2023-10-12 23:19:46 +1300

    Fixing syntax

--- launch/ignition_bridge.launch.py
+++ launch/ignition_bridge.launch.py
@@ -108,12 +108,12 @@ def generate_launch_description():
         }],
         arguments=[
             '/rplidar_front/scan@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan',
-            '/rplidar_front/scan/points@sensor_msgs/msg/PointCloud2' +'[ignition.msgs.PointCloudPacked'
+            '/rplidar_front/scan/points@sensor_msgs/msg/PointCloud2' +'[ignition.msgs.PointCloudPacked',
         ],
         remappings=[
             ('/rplidar_front/scan', '/scan'),
-            ('rplidar_front/scan/points', '/scan/points')
-            ('/zinger/link_sensor_lidar_rplidar_front/rplidar_front', 'link_sensor_lidar_rplidar_front')
+            ('rplidar_front/scan/points', '/scan/points'),
+            ('/zinger/link_sensor_lidar_rplidar_front/rplidar_front', 'link_sensor_lidar_rplidar_front'),
         ])
 
     # IMU bridge
